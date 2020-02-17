8b7606b44de4775e07f0ac5b8c17799ffb015ec0
Mark Pittaway <mark.pittaway@gmail.com>
2020-02-17 14:01:33 +1100

    [SDESK-4697] Increase max results and sort ascending for 'Content Filter' dropdowns

--- scripts/core/ui/components/generic-form/input-types/content-filter-single-value.tsx
+++ scripts/core/ui/components/generic-form/input-types/content-filter-single-value.tsx
@@ -6,7 +6,7 @@ import ng from 'core/services/ng';
 export const ContentFilterSingleValue = getSelectSingleValue(
     () =>
         ng.getService('api')
-            .then((api) => api('content_filters').query({max_results: 200}))
+            .then((api) => api('content_filters').query({max_results: 1000, sort: '[(\'name\', 1)]'}))
             .then((contentFilters: IRestApiResponse<IContentFilter>) =>
                 contentFilters._items.map(({_id, name}) => ({id: _id, label: name}))),
 );
