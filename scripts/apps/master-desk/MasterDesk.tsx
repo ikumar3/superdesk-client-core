36b46734ee0679326a3f7d6fc3fa3552f67a5408
Tomas <t.kikutis@gmail.com>
2020-09-04 17:54:14 +0200

    use enums for tabs

--- scripts/apps/master-desk/MasterDesk.tsx
+++ scripts/apps/master-desk/MasterDesk.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
-
-import { IDesk, IStage } from 'superdesk-api';
-
+import {IDesk, IStage} from 'superdesk-api';
 import {
     HeaderComponent,
     OverviewComponent,
     UsersComponent,
-    AssignmentsComponent
+    AssignmentsComponent,
 } from 'apps/master-desk/components';
+import {assertNever} from 'core/helpers/typescript-helpers';
+import {gettext} from 'core/utils';
+
+export enum IMasterDeskTab {
+    overview = 'overview',
+    users = 'users',
+    assignments = 'assignments',
+}
+
+export function getLabelForMasterDeskTab(tab: IMasterDeskTab): string {
+    switch (tab) {
+    case IMasterDeskTab.overview:
+        return gettext('Overview');
+    case IMasterDeskTab.users:
+        return gettext('Users');
+    case IMasterDeskTab.assignments:
+        return gettext('Assignments');
+    default:
+        return assertNever(tab);
+    }
+}
 
 interface IProps {
     desks: any;
@@ -18,7 +37,7 @@ interface IProps {
 interface IState {
     desks: Array<IDesk>;
     stages: Array<IStage>;
-    currentTab: string;
+    currentTab: IMasterDeskTab;
 }
 
 export class MasterDesk extends React.Component<IProps, IState> {
@@ -26,7 +45,7 @@ export class MasterDesk extends React.Component<IProps, IState> {
         super(props);
 
         this.state = {
-            currentTab: 'overview',
+            currentTab: IMasterDeskTab.overview,
             desks: [],
             stages: [],
         };
@@ -36,33 +55,45 @@ export class MasterDesk extends React.Component<IProps, IState> {
         const desks = this.props.desks;
 
         desks.initialize().then(() => {
-            this.setState({ desks: desks.desks._items });
-            this.setState({ stages: desks.deskStages });
+            this.setState({desks: desks.desks._items});
+            this.setState({stages: desks.deskStages});
         });
-    }
-
-    renderComponent() {
-        switch (this.state.currentTab) {
-            case 'overview':
-                return <OverviewComponent desks={this.state.desks} stages={this.state.stages} />;
-            case 'users':
-                return <UsersComponent desks={this.state.desks}
-                    apiService={this.props.api} deskService={this.props.desks} />;
-            case 'assignments':
-                return <AssignmentsComponent desks={this.state.desks} tasks={this.props.tasks} />;
-
-            default:
-                return <OverviewComponent desks={this.state.desks} stages={this.state.stages} />;
-        }
     }
 
     render() {
         return (
             <div className="sd-content-wrapper__main-content-area sd-main-content-grid">
-                <HeaderComponent onTabChange={(tab) => this.setState({ currentTab: tab })} />
+                <HeaderComponent onTabChange={(tab) => this.setState({currentTab: tab})} />
                 <div className="sd-main-content-grid__content">
                     <div className="sd-main-content-grid__content-inner">
-                        {this.renderComponent()}
+                        {(() => {
+                            switch (this.state.currentTab) {
+                            case IMasterDeskTab.overview:
+                                return (
+                                    <OverviewComponent
+                                        desks={this.state.desks}
+                                        stages={this.state.stages}
+                                    />
+                                );
+                            case IMasterDeskTab.users:
+                                return (
+                                    <UsersComponent
+                                        desks={this.state.desks}
+                                        apiService={this.props.api}
+                                        deskService={this.props.desks}
+                                    />
+                                );
+                            case IMasterDeskTab.assignments:
+                                return (
+                                    <AssignmentsComponent
+                                        desks={this.state.desks}
+                                        tasks={this.props.tasks}
+                                    />
+                                );
+                            default:
+                                return assertNever(this.state.currentTab);
+                            }
+                        })()}
                     </div>
                 </div>
             </div>
--- scripts/apps/master-desk/components/HeaderComponent.tsx
+++ scripts/apps/master-desk/components/HeaderComponent.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
-import { CheckButtonGroup, RadioButton } from 'superdesk-ui-framework/react';
-import { gettext } from 'core/utils';
+import {CheckButtonGroup, RadioButton} from 'superdesk-ui-framework/react';
+import {IMasterDeskTab, getLabelForMasterDeskTab} from '../MasterDesk';
 
 interface IProps {
     activeTab?: string;
-    onTabChange(tab: string): void;
+    onTabChange(tab: IMasterDeskTab): void;
 }
 
 interface IState {
-    currentTab: string;
+    currentTab: IMasterDeskTab;
 }
 
 export class HeaderComponent extends React.Component<IProps, IState> {
@@ -17,28 +17,30 @@ export class HeaderComponent extends React.Component<IProps, IState> {
         super(props);
 
         this.state = {
-            currentTab: 'overview',
+            currentTab: IMasterDeskTab.overview,
         };
 
         this.changeTab.bind(this);
     }
 
-    changeTab(tab: string) {
-        this.setState({ currentTab: tab });
+    changeTab(tab: IMasterDeskTab) {
+        this.setState({currentTab: tab});
         this.props.onTabChange(tab);
     }
 
     render() {
+        const tabs = Object.values(IMasterDeskTab).map((tab) => ({value: tab, label: getLabelForMasterDeskTab(tab)}));
+
         return (
             <div className="sd-main-content-grid__header">
                 <div className="subnav">
                     <div className="button-group button-group--left button-group--padded">
                         <CheckButtonGroup>
-                            <RadioButton value={this.state.currentTab} options={[
-                                { value: 'overview', label: gettext('Overview') },
-                                { value: 'users', label: gettext('Users') },
-                                { value: 'assignments', label: gettext('Assignments') },
-                            ]} onChange={(value) => this.changeTab(value)} />
+                            <RadioButton
+                                value={this.state.currentTab}
+                                options={tabs}
+                                onChange={(value: IMasterDeskTab) => this.changeTab(value)}
+                            />
                         </CheckButtonGroup>
                     </div>
                 </div>
